7d9cc90bb2381fafd4ef8838ced725e29347d7b0
Harsh <harshkc17@gmail.com>
2023-07-16 13:15:21 +0530

    Add project files.

---
+++ CalculateFuelEconomy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CalculateFuelEconomy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
